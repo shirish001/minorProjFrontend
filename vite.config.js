ec6a00f148817873ae91fdb1ab31e7a3eca26aef
Shirish Patel <shirish2001patel@gmail.com>
2024-12-10 19:04:08 +0530

    on render now

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/minorProjFrontend/", // in base put your repository name
+  base: "/", // in base put your repository name
 });
